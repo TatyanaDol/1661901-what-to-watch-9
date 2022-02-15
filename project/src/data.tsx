90600f8f47e0ed289e17e24402e87711e8a74371
Доль Татьяна Васильевна <t.v.dol@yandex.ru>
2022-02-15 20:17:11 +0300

    Начинаем программировать

---
+++ project/src/data.tsx
@@ -0,0 +1,6 @@
+import MovieCard from './components/movie-card/movie-card';
+
+const mockFilms = new Array(20).fill(<MovieCard/>);
+
+
+export default mockFilms;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const MovieData = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  year: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title = {MovieData.title}
+      genre = {MovieData.genre}
+      year = {MovieData.year}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
